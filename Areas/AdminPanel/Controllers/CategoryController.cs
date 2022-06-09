ec3f70027c4fa0dab95aa18fdadbc942fd62e041
fatima-28 <2n6gddb@code.edu.az>
2022-06-08 20:41:48 -0700

    date 20:40

--- Areas/AdminPanel/Controllers/CategoryController.cs
+++ Areas/AdminPanel/Controllers/CategoryController.cs
@@ -3,13 +3,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApp.DAL;
 
 namespace WebApp.Areas.AdminPanel.Controllers
 {
     [Area("AdminPanel")]
     public class CategoryController : Controller
     {
+
+        private AppDbContext _context { get; }
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
+        {
+            return View(_context.Categories.Where(ct=>!ct.IsDeleted));
+        }
+
+        public IActionResult Create()
         {
             return View();
         }
