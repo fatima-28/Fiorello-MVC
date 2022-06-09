21f1691d87e948cb0081b334f73c148e331f100f
fatima-28 <2n6gddb@code.edu.az>
2022-06-09 18:33:07 +0400

    iformfile, date:18 32

--- Areas/AdminPanel/Controllers/SliderController.cs
+++ Areas/AdminPanel/Controllers/SliderController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using WebApp.DAL;
+using WebApp.Models;
 
 namespace WebApp.Areas.AdminPanel.Controllers
 {
@@ -19,6 +21,34 @@ namespace WebApp.Areas.AdminPanel.Controllers
         public IActionResult Index()
         {
             return View(_context.Slides);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Slide slide)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            if (slide.Photo.Length / 1024 > 200) {
+
+                ModelState.AddModelError("Photo", "Image size error ");
+                return View();
+            
+            }
+            if (!slide.Photo.ContentType.Contains("image/"))
+            {
+                ModelState.AddModelError("Photo", "type error ");
+                return View();
+            }
+
+            
+            return Json(slide.Photo.FileName);
         }
     }
 }
